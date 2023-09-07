278b94bd83cbcce91f183c81e4c993ce3919394e
MohammedHassanLatif93 <mohammed.latif@digital.justice.gov.uk>
2023-09-07 13:56:41 +0100

    my comment

--- app.py
+++ app.py
@@ -240,5 +240,5 @@ def create_app(test_config=None):
 app = create_app()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8080, debug=True)
