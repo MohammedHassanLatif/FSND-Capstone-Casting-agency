f671281fe7d5e319fefcf9ae7db620860029c7dd
MohammedHassanLatif93 <mohammed.latif@digital.justice.gov.uk>
2023-09-07 21:11:54 +0100

    my comment

--- auth.py
+++ auth.py
@@ -4,15 +4,15 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
-
+from flask_sqlalchemy import SQLAlchemy
 
 # AUTH0_DOMAIN = 'hassanlatif.uk.auth0.com'
 # ALGORITHMS = ['RS256']
 # API_AUDIENCE = 'CastingAgencyAPI'
 
-AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN', 'hassanlatif.uk.auth0.com')
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
 ALGORITHMS = ['RS256']
-API_AUDIENCE = os.environ.get('API_AUDIENCE'  'CastingAgencyAPI')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 # AuthError Exception
 '''
